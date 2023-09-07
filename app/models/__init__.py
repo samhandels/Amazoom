1f7cc8c113f8d3a153c140f47c22ae854fb262ce
Sam Handelsman <samhandelsman@gmail.com>
2023-09-07 00:15:06 -0700

    push to main for render

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,6 @@
 from .db import db
 from .user import User
+from .product import Product
+from .shopping_cart import ShoppingCart
+from .shopping_cart_item import ShoppingCartItems
 from .db import environment, SCHEMA
