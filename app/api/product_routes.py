92c016f1a837a34157261df8f6d8c6b70ec16e4a
Sam Handelsman <samhandelsman@gmail.com>
2023-09-10 16:57:44 -0700

    added routes for delete prod and update

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -10,7 +10,7 @@ from flask_login import login_required, current_user # current_user.id
 from .aws_helper import upload_file_to_s3, get_unique_filename, remove_file_from_s3
 
 products = Blueprint("products", __name__)
-
+#get all prods
 @products.route("/")
 def get_products():
     """
@@ -36,7 +36,7 @@ def get_products():
     return response
 
 
-
+#get prod by id
 @products.route("/<int:id>")
 def get_single_product(id):
     """
@@ -57,7 +57,7 @@ def get_single_product(id):
     return response.to_dict()
 
 
-
+#post new product
 @products.route("/new", methods=["POST"])
 def create_product():
       """
@@ -95,3 +95,54 @@ def create_product():
             db.session.commit()
             return {"product": new_product.to_dict()}, 201
       return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+#update product
+@products.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_product(id):
+    product = Product.query.get(id)
+    if not product:
+        return {"errors": "Product not found"}, 404
+    if product.user_id != current_user.id:
+        return {"errors": "Unauthorized"}, 403
+
+    form = ProductForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    if form.validate_on_submit():
+        if 'image' in request.files:
+            image = request.files['image']
+            image.filename = get_unique_filename(image.filename)
+            upload = upload_file_to_s3(image)
+            if "url" not in upload:
+                return {"errors": "URL not in upload"}
+            remove_file_from_s3(product.image)
+            product.image = upload["url"]
+
+        product.name = form.data["name"]
+        product.price = form.data["price"]
+        product.description = form.data["description"]
+        product.quantity = form.data["quantity"]
+        product.category = form.data["category"]
+
+        db.session.commit()
+        return {"product": product.to_dict()}
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+
+#delete product
+@products.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_product(id):
+    product = Product.query.get(id)
+    if not product:
+        return {"errors": "Product not found"}, 404
+    if product.user_id != current_user.id:
+        return {"errors": "Unauthorized"}, 403
+
+    remove_file_from_s3(product.image)
+    db.session.delete(product)
+    db.session.commit()
+
+    return {"message": "Product deleted successfully"}
